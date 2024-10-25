36189cd85cf6cc01b37e8a08f975d507075c0b04
Sam Spell <llepsmas@gmail.com>
2024-10-24 23:17:27 -0500

    RedBlackTreeInsertion
    Red Black Insertion tested and completed

--- src/main/java/org/example/Trees/TreeVisualizerController.java
+++ src/main/java/org/example/Trees/TreeVisualizerController.java
@@ -52,6 +52,7 @@ public class TreeVisualizerController {
     private void initializeTrees() {
         trees = new HashMap<>();
         trees.put("Binary Search Tree", new BinarySearchTree<>());
+        trees.put("Red Black Tree", new RedBlack<>());
         //trees.put("AVL Tree", new AVLTree<>());
         //trees.put("Red-Black Tree", new RedBlackTree<>());
         //trees.put("Min Heap", new MinHeap<>());
@@ -67,6 +68,7 @@ public class TreeVisualizerController {
         treeTypeComboBox = new ComboBox<>();
         treeTypeComboBox.getItems().addAll(trees.keySet());
         treeTypeComboBox.setValue("Binary Search Tree");
+
 
         inputField = new TextField();
         insertButton = new Button("Insert");
